37f2520f08cde6ed52517b88a0ed1d2e8703b352
shoppingjei <shoppingjei@gmail.com>
2020-10-08 08:31:23 +0800

    合作商報價-程式異常排除

--- src/main/java/partner_h/quotecontract/main/dao/QuoteContractItemDao.java
+++ src/main/java/partner_h/quotecontract/main/dao/QuoteContractItemDao.java
@@ -5,9 +5,9 @@ import java.util.List;
 import partner_h.quotecontract.main.model.QuoteContractItemBean;
 
 public interface QuoteContractItemDao {
-	double findItemAmount(QuoteContractItemBean qcItemBean);
+//	double findItemAmount(QuoteContractItemBean qcItemBean);
 	
 	//依訂單號查詢多筆紀錄
-	List<QuoteContractItemBean> getQuote(int qcId);
+//	List<QuoteContractItemBean> getQuote(int qcId);
 	
 }
